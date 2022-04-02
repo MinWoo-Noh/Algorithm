8dd24976db0c63b043d4ada6eb9c4256cebc971a
= <redalsdn7@naver.com>
2022-04-02 21:18:23 +0900

    [BOJ] A+B

---
+++ src/BOJ/BOJ_1000.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+
+        System.out.println(a + b);
+
+    }
+}
